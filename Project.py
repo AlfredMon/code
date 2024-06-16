4722c4dd5d500a510ef438105f352b3b11ac4c8c
fordoosti <fordoosti@gmail.com>
2024-06-17 03:28:59 +0330

    This is the project for Code in place

---
+++ Project.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+window = tk.Tk()
+window.geometry("500x500")
+input_box1 = tk.Entry(window)
+input_box1.grid(row=1, column=0) # column is optional here since there's only 1 column
+input_box1.insert(0, "0") 
+
+label = tk.Label(window, text="How many books do you have?")
+label.grid(row=1, column=1)
+
+input_box2 = tk.Entry(window)
+input_box2.grid(row=3, column=0) 
+input_box2.insert(0, "0") 
+label = tk.Label(window, text="How many weeks do you have?")
+label.grid(row=3, column=1)
+
+input_box3 = tk.Entry(window)
+input_box3.grid(row=5, column=0)
+input_box3.insert(0, "0") 
+label = tk.Label(window, text="How long in a day do you study?")
+label.grid(row=5, column=1)
+
+
+window.mainloop()
